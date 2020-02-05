7f1d234f7031a211e55f98205ca2f75bd82aa8bc
SophieOrgana <admin@sophiesolo.biz>
2020-02-05 22:15:54 +0000

    Added job.elems

--- scraper.py
+++ scraper.py
@@ -7,6 +7,18 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find(id='ContentContainer')
+results = soup.find(id='ResultsContainer')
 
-print(results.prettify())
+job_elems = results.find_all('section', class_='card-content')
+
+for job_elem in job_elems:
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
+            continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
+# print(results.prettify())
